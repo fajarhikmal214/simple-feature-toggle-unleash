6ca4ab81f8bfcdc281eddf18ff86ee07c78b8e58
Fajar Hikmal <fajarhikmal214@gmail.com>
2023-08-24 14:06:20 +0700

    feat(watcher-service): add on and off commands

--- watcher-service/src/internal/example/usecase/usecase.ts
+++ watcher-service/src/internal/example/usecase/usecase.ts
@@ -22,12 +22,26 @@ class Usecase {
         this.help()
 
         this.status()
+        this.on()
+        this.off()
 
         this.telegramBot.launch()
         this.logger.info(`🚀 Bot launched`)
 
         this.telegramBot.action('help', (ctx) => {
             this.func_help(ctx)
+        })
+
+        this.telegramBot.action('status', (ctx) => {
+            this.func_status(ctx)
+        })
+
+        this.telegramBot.action('on', (ctx) => {
+            this.func_on(ctx)
+        })
+
+        this.telegramBot.action('off', (ctx) => {
+            this.func_off(ctx)
         })
     }
 
@@ -56,23 +70,55 @@ class Usecase {
         })
     }
 
+    private on() {
+        this.telegramBot.command('on', async (ctx) => {
+            this.logger.info(ctx.from)
+
+            await this.func_on(ctx)
+        })
+    }
+
+    private off() {
+        this.telegramBot.command('off', async (ctx) => {
+            this.logger.info(ctx.from)
+
+            await this.func_off(ctx)
+        })
+    }
+
     private async func_start(ctx: any) {
-        await this.telegramBot.telegram.sendMessage(
-            ctx.chat.id,
-            `Hello ${ctx.from.first_name}, Nice to meet you! 👋`,
-            {
-                reply_markup: {
-                    inline_keyboard: [
-                        [
-                            {
-                                text: 'Need Help ?',
-                                callback_data: 'help',
-                            },
-                        ],
+        let message = 'Simple Features Toggle Assist \n \n'
+        message += `I'm here to help you enable and disable external services according to your needs \n \n`
+        message += `Is there a particular feature you'd like to enable or enable at this time?`
+
+        await this.telegramBot.telegram.sendMessage(ctx.chat.id, message, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            text: 'Need Help ?',
+                            callback_data: 'help',
+                        },
                     ],
-                },
-            }
-        )
+                    [
+                        {
+                            text: 'STATUS',
+                            callback_data: 'status',
+                        },
+                    ],
+                    [
+                        {
+                            text: 'ACTIVATE',
+                            callback_data: 'on',
+                        },
+                        {
+                            text: 'DEACTIVATE',
+                            callback_data: 'off',
+                        },
+                    ],
+                ],
+            },
+        })
     }
 
     private async func_help(ctx: any) {
@@ -80,7 +126,10 @@ class Usecase {
 
         message += '- /start : Starts the bot \n'
         message += '- /help : List of commands \n'
+
         message += '- /status : Show external service status \n'
+        message += `- /on : Activate external service \n`
+        message += `- /off : Deactivate external service \n`
 
         message += '\n'
         message +=
@@ -92,24 +141,46 @@ class Usecase {
     private async func_status(ctx: any) {
         const now = new Date().toLocaleString()
 
-        // Mocking External Service health check (could be ping the External Service server)
+        // Mocking External Service health check (e.g. can ping External Service server)
         const isExternalServiceUp = await this.unleashClient.isEnable(
             this.statusExtServiceFeature
         )
 
+        let message = 'The following is the latest service status update \n \n'
         if (isExternalServiceUp) {
-            this.telegramBot.telegram.sendMessage(
-                ctx.chat.id,
-                `External Service is *Up*. Response: HTTP 200 - OK. Event occured on ${now}`,
-                { parse_mode: 'Markdown' }
-            )
-
-            return
+            message += `External Service is *Up*. Response: HTTP 200 - OK. Event occured on ${now} \n \n`
+        } else {
+            message += `External Service is *Down*. Response: HTTP 500 - Internal Server Error. Event occured on ${now} \n \n`
         }
+
+        const isEnableExtServiceOn = await this.unleashClient.isEnable(
+            this.enableExtServiceFeature
+        )
+
+        message += `Enable External Service : `
+        message += isEnableExtServiceOn ? `Active 🟢` : `Non-Active 🔴`
+
+        this.telegramBot.telegram.sendMessage(ctx.chat.id, message, {
+            parse_mode: 'Markdown',
+        })
+    }
+
+    private async func_on(ctx: any) {
+        await this.setFeatureToggle(this.enableExtServiceFeature, 'on')
 
         this.telegramBot.telegram.sendMessage(
             ctx.chat.id,
-            `External Service is *Down*. Response: HTTP 500 - Internal Server Error. Event occured on ${now}`,
+            `External Service successfully activated 🟢`,
+            { parse_mode: 'Markdown' }
+        )
+    }
+
+    private async func_off(ctx: any) {
+        await this.setFeatureToggle(this.enableExtServiceFeature, 'off')
+
+        this.telegramBot.telegram.sendMessage(
+            ctx.chat.id,
+            `External Service successfully deactivated 🔴`,
             { parse_mode: 'Markdown' }
         )
     }
@@ -117,15 +188,17 @@ class Usecase {
     private async func_health_check(ctx: any) {
         const now = new Date().toLocaleString()
 
+        // Mocking External Service health check (e.g. can ping External Service server)
         const isExternalServiceUp = await this.unleashClient.isEnable(
             this.statusExtServiceFeature
         )
+
         const isEnableExtServiceOn = await this.unleashClient.isEnable(
             this.enableExtServiceFeature
         )
 
         if (isExternalServiceUp && !isEnableExtServiceOn) {
-            await this.setFeatureToggle(this.enableExtServiceFeature, 'off')
+            await this.setFeatureToggle(this.enableExtServiceFeature, 'on')
 
             this.telegramBot.telegram.sendMessage(
                 ctx.chat.id,
@@ -135,7 +208,7 @@ class Usecase {
         }
 
         if (!isExternalServiceUp && isEnableExtServiceOn) {
-            await this.setFeatureToggle(this.enableExtServiceFeature, 'on')
+            await this.setFeatureToggle(this.enableExtServiceFeature, 'off')
 
             this.telegramBot.telegram.sendMessage(
                 ctx.chat.id,
@@ -146,9 +219,10 @@ class Usecase {
     }
 
     private async func_health_check_gradually(ctx: any) {
+        // Run every 1 minute
         setInterval(async () => {
             await this.func_health_check(ctx)
-        }, 1 * 60 * 1000) // Run every 1 minute
+        }, 1 * 60 * 1000)
     }
 
     private async setFeatureToggle(feature: string, status: 'on' | 'off') {
@@ -164,6 +238,10 @@ class Usecase {
             }
 
             await axios.post(url, {}, header)
+
+            this.logger.info(
+                `Set feature toggle ${feature} to ${status} successfully`
+            )
         } catch (error: any) {
             this.logger.error(error.message)
         }
